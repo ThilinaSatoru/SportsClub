1b7c5e5d54a26c8d4ef1a0628262ed64da26bbc1
Thilina Satoru <satoru.thilina@gmail.com>
2023-10-08 15:00:26 +0530

    event validation

--- PHP/event.php
+++ PHP/event.php
@@ -10,33 +10,64 @@ $e_type = "";
 $e_description = "";
 
 if (isset($_POST['reg_event'])) {
-	$e_fname = $_POST['fname'];
+    $e_fname = $_POST['fname'];
     $e_lname = $_POST['lname'];
     $e_email = $_POST['email'];
     $e_title = $_POST['title'];
     $e_type = $_POST['type'];
     $e_description = $_POST['description'];
-	
+
     donate($e_fname, $e_lname, $e_email, $e_title, $e_type, $e_description);
 }
 
-function donate($fname, $lname, $email, $title, $type, $description) {
-	global $con;
+function validate_events($e_fname, $e_lname, $e_email, $e_title, $e_type, $e_description)
+{
 
-	$query = "INSERT INTO event (fname, lname, email, title, type, description) VALUES('$fname', '$lname', '$email', '$title', '$type', '$description')";
-	mysqli_query($con, $query);
-
-    echo "<script>";
-    if (headers_sent()) {
-        echo "alert('Something Went Wrong!');";
-        echo "</script>";
-        die("Redirect failed. Please click on this link: <a href=../Events.php>");
-    }
-    else{
-        echo "alert('Event Added Successfully.');";
-        echo "window.location = '../Events.php';"; // redirect with javascript, after page loads
-        echo "</script>";
+    if (!preg_match('/^[A-Za-z\s]+$/', $e_fname)) {
+        $errors["First Name"] = "Invalid First Name.";
     }
 
-	mysqli_close($con);	
+    if (!preg_match('/^[A-Za-z\s]+$/', $e_lname)) {
+        $errors["First Name"] = "Invalid Last Name.";
+    }
+
+
+    /////// If there are validation errors, display them //////////////////////////////////////////////
+    if (!empty($errors)) {
+        foreach ($errors as $field => $error) {
+            echo "<script>";
+            echo "alert('" . strtoupper($field) . " : " . $error . "');";
+            echo "</script>";
+        }
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function donate($fname, $lname, $email, $title, $type, $description)
+{
+    global $con;
+
+    if (validate_events($fname, $lname, $email, $title, $type, $description)) {
+        $query = "INSERT INTO event (fname, lname, email, title, type, description) VALUES('$fname', '$lname', '$email', '$title', '$type', '$description')";
+        mysqli_query($con, $query);
+
+        echo "<script>";
+        if (headers_sent()) {
+            echo "alert('Something Went Wrong!');";
+            echo "</script>";
+            die("Redirect failed. Please click on this link: <a href=../Events.php>");
+        } else {
+            echo "alert('Event Added Successfully.');";
+            echo "window.location = '../Events.php';"; // redirect with javascript, after page loads
+            echo "</script>";
+        }
+
+        mysqli_close($con);
+    } else {
+        echo "<script>";
+        echo "window.location = '../SportItems.php#request_form';"; // redirect with javascript, after page loads
+        echo "</script>";
+    }
 }
